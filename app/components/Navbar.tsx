8ac6e02bf418b19366defea7ae0de0770697a392
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2024-12-01 17:29:34 +0530

    completed the root layout.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = async () => {
     const session = await auth()    
 
   return (
-    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans font-bold">
         <nav className='flex justify-between items-center'>
             <Link href="/">
                 <Image src="/logo.png" alt="logo" height={30} width={144} />
