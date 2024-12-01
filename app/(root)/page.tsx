e5bbd588d8b3afe1bf91624718f35835c64679aa
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2024-12-01 18:03:43 +0530

    fixed pink hero section issue

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="heading">YC Directory</h1>
+      <section className="pink_container">
+        <h1 className="heading">Pitch Your Startup, <br/> Connect with Entrepreneurs</h1>
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Notices in Virtual Competitions !</p>
+      </section>
     </>
   );
 }
