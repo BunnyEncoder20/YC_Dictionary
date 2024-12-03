078bd100288d3e00d80cfd0702abb4ad8f16d52c
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2024-12-03 13:24:46 +0530

    search is working now

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -12,15 +12,18 @@ import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 
 
+
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
 
+  // fetching search query
   const query = (await searchParams).query;
+  const params = { search: query || null }
 
   // fetch data from sanity 
   // const posts = await client.fetch(STARTUPS_QUERY)
 
   // new fetch for Sanity Live api (revalidates pages everytime new data is added)
-  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY })
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params })
   console.log(JSON.stringify(posts , null, 2))
 
   return (
