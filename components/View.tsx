5dffd912bb32a25ff57e8f1a7b7e973061ceb1f9
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2024-12-03 19:08:26 +0530

    sanity write client implemented. View write working as expected

--- components/View.tsx
+++ components/View.tsx
@@ -6,16 +6,23 @@ import Ping from '@/components/Ping'
 // Sanity imports
 import { client } from '@/sanity/lib/client'
 import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries'
+import { writeClient } from '@/sanity/lib/write-client'
 
 // utils imports
 import { formatViews } from '@/lib/utils'
 
+import { unstable_after as after } from 'next/server'
 // current component 
 const View = async ({ id }: { id: string }) => {
 
     const { views: totalViews } = await client.withConfig({ useCdn: false }).fetch(STARTUP_VIEWS_QUERY, {id})
 
-    // TODO: Add live view updation
+    // update views each time page is visited
+    after(async () => await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+    )
 
 
   return (
